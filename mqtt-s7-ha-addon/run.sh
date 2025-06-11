bf25872aa97b06462c63fb5c6156271c7fa6abbe
p0nch00 <joshi@creativemen.de>
2025-06-11 07:22:49 +0000

    fix dockerfile

--- mqtt-s7-ha-addon/run.sh
+++ mqtt-s7-ha-addon/run.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 CONFIG_PATH="/ha-cfg/mqtts7/config.json"
-APP_CONFIG="/app/src/config.json"
+APP_CONFIG="config.json"
 
 cp $CONFIG_PATH $APP_CONFIG
-
-ls app/src
-
-ls -la
-
-cat $APP_CONFIG
 
 exec npm start
