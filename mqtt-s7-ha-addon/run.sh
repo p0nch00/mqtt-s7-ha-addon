1853dfb12bfc10d03862f09863ed6883124b4c58
p0nch00 <joshi@creativemen.de>
2025-06-11 07:12:03 +0000

    fix dockerfile

--- mqtt-s7-ha-addon/run.sh
+++ mqtt-s7-ha-addon/run.sh
@@ -3,11 +3,8 @@
 CONFIG_PATH="/ha-cfg/mqtts7/config.json"
 APP_CONFIG="/app/src/config.json"
 
-# Create default config if not exists
-if [ ! -f "$CONFIG_PATH" ]; then
-  echo '{"mqtt": {"host": "localhost", "port": 1883}, "s7": {"ip": "192.168.0.1"}}' > "$CONFIG_PATH"
-fi
-
 ln -sf "$CONFIG_PATH" "$APP_CONFIG"
+
+ls -la
 
 exec npm start
