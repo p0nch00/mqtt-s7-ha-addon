3f3f21fff313426cb67e92e5fe291cef032bda9c
p0nch00 <joshi@creativemen.de>
2025-06-13 08:01:15 +0000

    fix dockerfile

--- mqtt-s7-ha-addon/bin/devices/lightbutton.js
+++ mqtt-s7-ha-addon/bin/devices/lightbutton.js
@@ -56,7 +56,8 @@ module.exports = class devLightButton extends device {
 		super.rec_mqtt_data(attr, data, (error) => {
 			// callback function of attribute when write was finished
 			console.log("CALLBACK!!!!");
-			console.log(this.get_plc_address(attr))
+			console.log(this.get_plc_address(attr));
+			console.log(this.attributes[attr].state);
 		});
 	}
 
